6c77edbd6b6e9f8759aad8c8a76d4f3fef1db9b3
Ubuntu <ubuntu@ip-172-31-10-60.us-west-1.compute.internal>
2015-12-12 23:55:34 +0000

    Make docker container run as daemon

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t skibuddy-live .
-docker run -it --rm --name skibuddy-server -p 8282:8282 skibuddy-live
+docker run -dt --name skibuddy-server -p 8282:8282 skibuddy-live
